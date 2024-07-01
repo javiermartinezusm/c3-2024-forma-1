094abdb3b44fc39e1fcc740d1c78d1394fc73178
javiermartinezusm <javier.martinezf@usm.cl>
2024-07-01 19:50:06 -0400

    feat: ultimo caso agregado y testeado

--- src/domain/cities/use_cases/getCities.js
+++ src/domain/cities/use_cases/getCities.js
@@ -5,17 +5,24 @@ exports.getAllCitiesUseCase = (ctx) => {
     return ctx
 }
 
-exports.getCitiesByCountryUseCase = (ctx) => { //ESTE ES
-    const ciudades = citiesRepository.searchCitiesByCountryName(ctx.params.country)
-    if(ciudades.length!=0){
-        ctx.body = ciudades
+exports.getCitiesByCountryUseCase = (ctx) => {
+    const country = ctx.params.country;
+    const ciudades = citiesRepository.searchCitiesByCountryName(country);
+    const regex = /[0-9]/
+    if (regex.test(country)) {
+        ctx.body = { message: "Solo se aceptan caracteres no numéricos" };
+        ctx.status = 400;
+    } else {
+        if (ciudades.length !== 0) {
+            ctx.body = ciudades;
+        } else {
+            ctx.body = { message: "No se encontraron ciudades para el país ingresado" };
+        }
+        ctx.status = 200;
     }
-    else{
-        ctx.body = {message: "No se encontraron ciudades para el país ingresado"}
-    }
-    ctx.status = 200
-    return ctx
-}
+    return ctx;
+};
+
 
 exports.getCitiesByCityNameAndCountryUseCase = (ctx) => {
     ctx.body = citiesRepository.searchCityByCityNameAndCountry(ctx.params.city, ctx.params.country)
--- tests/domain/cities/use_cases/GETCITIESbyCountryUseCase.test.js
+++ tests/domain/cities/use_cases/GETCITIESbyCountryUseCase.test.js
@@ -1,56 +1,52 @@
 const { getCitiesByCountryUseCase } = require('../../../../src/domain/cities/use_cases/getCities');
-
-jest.mock('../../../../src/domain/cities/repository/worldCitiesRespository.js', () => ({
-  searchCitiesByCountryName: jest.fn()
-}));
+jest.mock('../../../../src/domain/cities/repository/worldCitiesRespository.js');
 
 const citiesRepository = require('../../../../src/domain/cities/repository/worldCitiesRespository.js');
 
 describe('getCitiesByCountryUseCase', () => {
-  it('debería retornar la lista de ciudades con status 200 para Chile', () => {
-    const country = 'Chile';
-    const cities = ['Santiago', 'Valparaíso', 'Concepción'];
-    citiesRepository.searchCitiesByCountryName.mockReturnValue(cities);
-    const ctx = {
-      params: {
-        country
-      },
-      body: null
-    };
-    getCitiesByCountryUseCase(ctx);
-    expect(citiesRepository.searchCitiesByCountryName).toHaveBeenCalledWith(country);
-    expect(ctx.body).toEqual(cities);
-    expect(ctx.status).toEqual(200);
-  });
+    beforeEach(() => {
+        jest.clearAllMocks(); // Resetea todos los mocks antes de cada test
+    });
 
-  it('debería retornar el mensaje especial con status 200 para Wakanda', () => {
-    const country = 'Wakanda';
-    const cities = [];
-    citiesRepository.searchCitiesByCountryName.mockReturnValue(cities);
-    const ctx = {
-      params: {
-        country
-      },
-      body: null
-    };
-    getCitiesByCountryUseCase(ctx);
-    expect(citiesRepository.searchCitiesByCountryName).toHaveBeenCalledWith(country);
-    expect(ctx.body).toEqual({ message: 'No se encontraron ciudades para el país ingresado' });
-    expect(ctx.status).toEqual(200);
-  });
-  it('debería retornar status 400 y body con instrucciones cuando se ingresan numero en el pais', () => {
-    const country = 'R2D2';
-    const cities = [];
-    citiesRepository.searchCitiesByCountryName.mockReturnValue(cities);
-    const ctx = {
-      params: {
-        country
-      },
-      body: null
-    };
-    getCitiesByCountryUseCase(ctx);
-    expect(citiesRepository.searchCitiesByCountryName).toHaveBeenCalledWith(country);
-    expect(ctx.body).toEqual({ message: "Solo se aceptan caracteres no numéricos"});
-    expect(ctx.status).toEqual(400);
-  });
+    it('debería retornar la lista de ciudades con status 200 para chile', () => {
+        const country = 'chile';
+        const cities = ['Santiago', 'Valparaíso', 'Concepción'];
+        citiesRepository.searchCitiesByCountryName.mockReturnValue(cities);
+        const ctx = {
+            params: { country },
+            body: null
+        };
+        getCitiesByCountryUseCase(ctx);
+        expect(citiesRepository.searchCitiesByCountryName).toHaveBeenCalledWith(country);
+        expect(ctx.body).toEqual(cities);
+        expect(ctx.status).toEqual(200);
+    });
+
+    it('debería retornar el mensaje especial con status 200 para Wakanda', () => {
+        const country = 'Wakanda';
+        const cities = [];
+        citiesRepository.searchCitiesByCountryName.mockReturnValue(cities);
+        const ctx = {
+            params: { country },
+            body: null
+        };
+        getCitiesByCountryUseCase(ctx);
+        expect(citiesRepository.searchCitiesByCountryName).toHaveBeenCalledWith(country);
+        expect(ctx.body).toEqual({ message: 'No se encontraron ciudades para el país ingresado' });
+        expect(ctx.status).toEqual(200);
+    });
+
+    it('debería retornar status 400 y body con instrucciones cuando se ingresan números en el país', () => {
+        const country = 'R2D2';
+        const cities = [];
+        citiesRepository.searchCitiesByCountryName.mockReturnValue(cities);
+        const ctx = {
+            params: { country },
+            body: null
+        };
+        getCitiesByCountryUseCase(ctx);
+        expect(citiesRepository.searchCitiesByCountryName).toHaveBeenCalledWith(country);
+        expect(ctx.body).toEqual({ message: "Solo se aceptan caracteres no numéricos" });
+        expect(ctx.status).toEqual(400);
+    });
 });
