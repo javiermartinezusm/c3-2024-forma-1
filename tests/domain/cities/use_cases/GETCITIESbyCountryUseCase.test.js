82e55c4e107e1a4cf71e0686704a13c79dd83686
javiermartinezusm <javier.martinezf@usm.cl>
2024-07-01 19:22:09 -0400

    feat: Test para pais con numeros

--- tests/domain/cities/use_cases/GETCITIESbyCountryUseCase.test.js
+++ tests/domain/cities/use_cases/GETCITIESbyCountryUseCase.test.js
@@ -7,7 +7,7 @@ jest.mock('../../../../src/domain/cities/repository/worldCitiesRespository.js', 
 const citiesRepository = require('../../../../src/domain/cities/repository/worldCitiesRespository.js');
 
 describe('getCitiesByCountryUseCase', () => {
-  it('debería retornar la lista de ciudades de Chile', () => {
+  it('debería retornar la lista de ciudades con status 200 para Chile', () => {
     const country = 'Chile';
     const cities = ['Santiago', 'Valparaíso', 'Concepción'];
     citiesRepository.searchCitiesByCountryName.mockReturnValue(cities);
@@ -23,7 +23,7 @@ describe('getCitiesByCountryUseCase', () => {
     expect(ctx.status).toEqual(200);
   });
 
-  it('debería retornar una lista vacía para Wakanda', () => {
+  it('debería retornar el mensaje especial con status 200 para Wakanda', () => {
     const country = 'Wakanda';
     const cities = [];
     citiesRepository.searchCitiesByCountryName.mockReturnValue(cities);
@@ -37,5 +37,20 @@ describe('getCitiesByCountryUseCase', () => {
     expect(citiesRepository.searchCitiesByCountryName).toHaveBeenCalledWith(country);
     expect(ctx.body).toEqual({ message: 'No se encontraron ciudades para el país ingresado' });
     expect(ctx.status).toEqual(200);
+  });
+  it('debería retornar status 400 y body con instrucciones cuando se ingresan numero en el pais', () => {
+    const country = 'R2D2';
+    const cities = [];
+    citiesRepository.searchCitiesByCountryName.mockReturnValue(cities);
+    const ctx = {
+      params: {
+        country
+      },
+      body: null
+    };
+    getCitiesByCountryUseCase(ctx);
+    expect(citiesRepository.searchCitiesByCountryName).toHaveBeenCalledWith(country);
+    expect(ctx.body).toEqual({ message: "Solo se aceptan caracteres no numéricos"});
+    expect(ctx.status).toEqual(400);
   });
 });
